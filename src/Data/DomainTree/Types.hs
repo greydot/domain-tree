9a79af1c82e7901d57525f45abf3eb52d52dbb51
Lana Black <lanablack@amok.cc>
2018-10-24 15:02:03 +0000

    Add more instances and functions.

--- src/Data/DomainTree/Types.hs
+++ src/Data/DomainTree/Types.hs
@@ -6,7 +6,13 @@
 {-# LANGUAGE TypeFamilies #-}
 module Data.DomainTree.Types where
 
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Lazy as LazyByteString
+import Data.Foldable (toList)
 import Data.List (partition)
+import qualified Data.Text as Text
+import qualified Data.Text.Lazy as LazyText
+import Data.Word (Word8)
 
 class (Eq s, Eq (Elem s)) => Sequence s where
   type Elem s
@@ -22,6 +28,22 @@ instance Eq a => Sequence [a] where
       where
         cons ~(h, t) = h:t
 
+instance Sequence ByteString.ByteString where
+  type Elem ByteString.ByteString = Word8
+  split = ByteString.split
+
+instance Sequence LazyByteString.ByteString where
+  type Elem LazyByteString.ByteString = Word8
+  split = LazyByteString.split
+
+instance Sequence Text.Text where
+  type Elem Text.Text = Char
+  split e = Text.split (== e)
+
+instance Sequence LazyText.Text where
+  type Elem LazyText.Text = Char
+  split e = LazyText.split (== e)
+
 data DomainTree k a = DomainTree { sepElem :: Elem k
                                  , domainTrees :: [Tree k a]
                                  } deriving (Functor, Foldable, Traversable)
@@ -36,10 +58,9 @@ data Tree k a = Tree { treeKey :: !k
 empty :: Sequence k => Elem k -> DomainTree k a
 empty s = DomainTree s []
 
-insert :: Sequence k => k -> a -> DomainTree k a -> DomainTree k a
-insert k v (DomainTree sep ts) = DomainTree sep $ go kseq ts
+insertChunks :: Eq k => [k] -> a -> DomainTree k a -> DomainTree k a
+insertChunks ks v (DomainTree sep ts) = DomainTree sep $ go ks ts
   where
-    kseq = split sep k
     go [] _ = error "empty key sequence"
     go (c:cs) ts = let (m,o) = partition (\n -> treeKey n == c) ts
                    in case m of
@@ -48,10 +69,16 @@ insert k v (DomainTree sep ts) = DomainTree sep $ go kseq ts
                         [] | null cs -> Tree c (pure v) []:o
                            | otherwise -> Tree c Nothing (go cs []):o
                         _ -> error "multiple nodes with the same key found"
+{-# INLINE insertChunks #-}
+
+insert :: Sequence k => k -> a -> DomainTree k a -> DomainTree k a
+insert k v t@(DomainTree sep _) = insertChunks (split sep k) v t
+{-# INLINE insert #-}
 
 fromList :: Sequence k => Elem k -> [(k,a)] -> DomainTree k a
 fromList sep = foldr (\(k,v) -> insert k v) e
   where e = empty sep
+{-# INLINE fromList #-}
 
 lookup :: Sequence k => k -> DomainTree k a -> Maybe a
 lookup key (DomainTree sep trees) = go kseq trees
